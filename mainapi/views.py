499c96f757525719d9895b2a25e18c037c6955dc
venomega <guilleps92@gmail.com>
2025-01-08 23:27:10 -0500

    se termino la logica de enviar msg de texto, y parte de la lectura

--- mainapi/views.py
+++ mainapi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from mainapi import models
+from .models import User
 import json
 import code
 # Create your views here.
@@ -21,14 +23,54 @@ def send_message(request):
         msg = json.loads(request.body.decode())
     except json.JSONDecodeError:
         return JsonResponse({"status":"err","body":"Error leyendo JSON"})
-
     msg_type = msg["type"]
     msg_body = msg["text"]
+    token = msg["token"]
+    room = msg["room"]
+    try:
+        remitente = models.User.objects.get(token=token) 
+    except models.User.DoesNotExist:
+        return JsonResponse({"status":"err","body":"Token invalido"})
+    try:
+        room = models.Room.objects.get(nombre=room) 
+    except models.Room.DoesNotExist:
+        return JsonResponse({"status":"err","body":"Token invalido"})
     if msg_type & 1 == 1: # text
+        models.Message(remitente=remitente, text=msg_body, room=room, message_type=msg_type ).save()
+    if msg_type & 2 == 2: # image
+        #models.Message(remitente=remitente, text=msg_body, room=room, message_type=msg_type ).save()
         pass
-
+    if msg_type & 3 == 3: # audio
+        #models.Message(remitente=remitente, text=msg_body, room=room, message_type=msg_type ).save()
+        pass
     return JsonResponse({"status":"ok"})
 
 
+def read_message(request):
+    if request.method != "GET":
+        return JsonResponse({"status":"err","body":"Method have to be GET"})
+    msg = []
+    filters = {}
+    try:
+        token = msg["token"]
+        try:
+            remitente = models.User.objects.get(token=token) 
+        except models.User.DoesNotExist:
+            return JsonResponse({"status":"err","body":"Token invalido"})
+        room = msg["room"]
+        try:
+            room = models.Room.objects.get(nombre=room) 
+        except models.Room.DoesNotExist:
+            return JsonResponse({"status":"err","body":"Token invalido"})
+        date = msg["date"]
+        msg_id = msg["msg_id"]
+        if msg_id != "":
+            filters["id__gt"] = msg_id
+        if date != "":
+            filters["date__gt"] = date
+
+    except:
+        return JsonResponse({"status":"err","body":"Falta algun campo obligatorio"})
+    return JsonResponse({"status":"ok"})
 
 
